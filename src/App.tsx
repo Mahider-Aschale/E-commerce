3119508b551a778de92cbd4e5ba6ead609e47941
Mahider Aschale <mahideraschale@gmail.com>
2026-04-12 17:01:38 +0300

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { Routes, Route } from "react-router-dom"
 import HomePage from './pages/homePage';
-import Women from './compont/women'
-import Men from './compont/men'
 import ProductList from './pages/productList';
 import ProductDetail from './pages/productDetail';
 import CartPage from './pages/cartPage'
 import CheckoutPage from './pages/checkout'
-import Autumn from './pages/autumn'
+
 import AccountPage from './pages/account'
 import './App.css'
 
@@ -19,9 +17,6 @@ return(
       <Route path="/product/:id" element={<ProductDetail/>}/>
       <Route path="/cart" element={<CartPage />} />
       <Route path="/checkout" element={<CheckoutPage />} /> 
-      <Route path="/women" element={<Women/>} /> 
-      <Route path="/men" element={<Men/>}/>
-      <Route path="/autumncollection" element={<Autumn/>}/>
       <Route path="/account" element={<AccountPage/>}/>
     </Routes>
      </>
--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-  <BrowserRouter> {/* Wrap your app here */}
+  <BrowserRouter>
   <CartProvider>
       <App />
     </CartProvider>
